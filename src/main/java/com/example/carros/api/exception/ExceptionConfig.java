29509af92bad8befc9545bb98053bfc3970c04bb
AVT Estudos <andretamayoshi@gmail.com>
2019-10-28 21:37:26 -0300

    53-ResponseEntityExceptionHandler

--- src/main/java/com/example/carros/api/exception/ExceptionConfig.java
+++ src/main/java/com/example/carros/api/exception/ExceptionConfig.java
@@ -1,14 +1,20 @@
 package com.example.carros.api.exception;
 
+import java.io.Serializable;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 //Anotação que permite que a classe intercepta 
 //eventos do rest inclusive as exceções  
 @RestControllerAdvice
-public class ExceptionConfig {
+public class ExceptionConfig extends ResponseEntityExceptionHandler {
 	
 	/*
 	 * Fazendo o mapeamento da exceção para o Spring direcionar
@@ -32,5 +38,25 @@ public class ExceptionConfig {
 		
 		return ResponseEntity.badRequest().build();
 		
+	}
+	
+	@Override
+	public ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+			HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		return new ResponseEntity<>(new ExceptionError("Operação não permitida."), HttpStatus.METHOD_NOT_ALLOWED);
+	}	
+}
+
+//Classe criada para permitir que o erro seja enviado no formato JSON
+class ExceptionError implements Serializable{
+	private String error;
+	
+	ExceptionError(String error) {
+		this.error = error;
+	}
+	
+	public String getError() {
+		return error;
 	}
 }
