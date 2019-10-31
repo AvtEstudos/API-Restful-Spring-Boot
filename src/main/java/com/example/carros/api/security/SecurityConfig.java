3e73ee33c37fe432d7c2ef058effc8a81288244b
AVT Estudos <andretamayoshi@gmail.com>
2019-10-30 22:42:16 -0300

    57-Basic Authentication

---
+++ src/main/java/com/example/carros/api/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.carros.api.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//Como é uma classe de configuração ativamos a anotação @Configuration 
+@Configuration
+//Habilitando a segurança via Web
+@EnableWebSecurity
+//Extendendo a classe de configuração do spring
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		//.anyRequest().authenticated(): Define que todo request deverá ser autenticado.		
+		//.formLogin().and(): Com a retirada dessa linha não teremos a tela de login
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()			
+			.httpBasic();
+	}
+
+}
